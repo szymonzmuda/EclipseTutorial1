a36a815232414447768bc00ebe79ad4ea1a7790e
Szymon Zmuda <szm@commit.pl>
2022-12-01 14:45:19 +0100

    TestNG hello world

---
+++ testproject.szm/src/test/java/testproject/szm/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package testproject.szm;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld() {
+		System.out.println("...test hello world...");
+	}
+
+}
